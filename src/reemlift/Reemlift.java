5903cde693b5389c22bfae96e9fc16420ed4842d
Donovan S. Allen <dono.allen@gmail.com>
2014-11-11 12:56:09 -0700

    wipe for fresh start

--- src/reemlift/Reemlift.java
+++ src/reemlift/Reemlift.java
@@ -6,7 +6,6 @@
 
 package reemlift;
 
-import reemlift.Game.Game;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -16,10 +15,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import reemlift.utils.DBmanager;
-import reemlift.utils.Index;
-import reemlift.utils.Menu;
-import reemlift.utils.WaitBar;
+
 
 /**
  *
@@ -28,11 +24,8 @@ import reemlift.utils.WaitBar;
 public class Reemlift {
     private static JFrame frame;
     private static Container fPain;
-    private static HashMap<String, Menu> menus = new HashMap<>();
-    private static WaitBar bar;
-    public static Game main;
+
     private static void Setup(){
-        Index.Setup();
         frame = new JFrame("ReemLift");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JLabel emptyLabel = new JLabel("");
@@ -41,57 +34,10 @@ public class Reemlift {
         fPain.add(emptyLabel, BorderLayout.WEST);
         frame.pack();
         frame.setVisible(true);
-        Menu mhold = new Menu(frame, new Menu.OptionClickEventHandler() {
-            @Override
-            public void onOptionClick(Menu.OptionClickEvent event) {
-                if(event.getName().equalsIgnoreCase("new game")){
-                    main = new Game(frame);
-                    main.Start();
-                }else if(event.getName().equalsIgnoreCase("load game")){
-                    Menu mhold = new Menu(frame, new Menu.OptionClickEventHandler() {
-                        @Override
-                        public void onOptionClick(Menu.OptionClickEvent event) {
-                            Game game = DBmanager.Load(event.getName()).asGame(frame);
-                            game.Start();
-                        }
-                    })
-                    .SetTitle("Load Save");
-                    int pos = 1;
-                    for(String s : DBmanager.getSaves()){
-                        mhold.addOP(pos, s, "Load save " + s);
-                        pos++;
-                    }
-                    mhold.show(BorderLayout.NORTH);
-                }else{
-                    WindowEvent wev = new WindowEvent(frame, WindowEvent.WINDOW_CLOSING);
-                    Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(wev);
-                }
-            }
-        })
-        .addOP(1, "New Game", "A fresh start")
-        .addOP(2, "Load Game", "Pick up where you left off")
-        .addOP(3, "Exit", "nooooo")
-        .SetTitle("Main Menu");
-        menus.put("main", mhold);
-        bar = new WaitBar(frame);
+        
     }
     public static void main(String[] args) {
         Setup();
         frame.pack();
-        menus.get("main").show(BorderLayout.NORTH);
     }
 }
-
-
-//                if(event.getName().equalsIgnoreCase("kill")){
-//                    event.setWillClose(false);
-//                    event.setWillDisable(true);
-//                    bar.Start(new WaitBar.OptionClickEventHandler() {
-//                        @Override
-//                        public void onOptionClick(WaitBar.OptionClickEvent event) {
-//                            menus.get("mn_kill").show();
-//                        }
-//                    });
-//                }else{
-//                    menus.get("mn_live").show();
-//                }
