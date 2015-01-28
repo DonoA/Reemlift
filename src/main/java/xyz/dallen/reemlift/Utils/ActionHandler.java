ecd3f942ee44354296dc9670b5b583606be7438c
Donovan S. Allen <dono.allen@gmail.com>
2015-01-28 16:26:46 -0700

    quick fix for actions

--- src/main/java/xyz/dallen/reemlift/Utils/ActionHandler.java
+++ src/main/java/xyz/dallen/reemlift/Utils/ActionHandler.java
@@ -192,10 +192,10 @@ public class ActionHandler extends KeyAdapter/*implements ActionListener*/{
         @Override
         public void actionPerformed(ActionEvent ev){
                 if(!cooling){
-                    Shot holder = new Shot(0, 0, DBmanager.getPlayer().getDir());
+                    Shot holder = new Shot(0, 0, DBmanager.getPlayer().getLocation().getDir());
                     Number x = DBmanager.getPlayer().getHitbox().getCenterX() - (holder.getHitbox().width/2);
                     Number y = DBmanager.getPlayer().getHitbox().getCenterY() - (holder.getHitbox().height/2);
-                    Shot s = new Shot(x.intValue(), y.intValue(), DBmanager.getPlayer().getDir());
+                    Shot s = new Shot(x.intValue(), y.intValue(), DBmanager.getPlayer().getLocation().getDir());
                     DBmanager.MovingShots.add(s);
                     DBmanager.ForRender.add(s);
                     cooling = true;
@@ -213,7 +213,7 @@ public class ActionHandler extends KeyAdapter/*implements ActionListener*/{
     private static Action INTERACT = new AbstractAction("Interact"){
         @Override
         public void actionPerformed(ActionEvent ev){
-                if(LevelDBmanager.CurrLevel.getNpcDB().inRange(DBmanager.getPlayer().getHitbox())!= null){
+                if(LevelDBmanager.CurrLevel.getNpcDB().inRange(DBmanager.getPlayer().getLocation())!= null){
                     //interact with npc
                 }else{
                     //open inventory
